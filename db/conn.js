8fa3482665c943b78bbf42cf43b82eccbbf05ce6
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-04-17 21:26:08 -0400

    fix conn

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgress://localhost/grace_shopper_db');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/grace_shopper_db');
 
 module.exports = conn;
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const app = require('./server/app')
+const app = require('./server/app');
+
+const db = require('./db');
+
+db.syncAndSeed()
+  .then(()=>{console.log('seeded')})
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
