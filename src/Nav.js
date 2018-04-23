3e68334a910295d6be895e1b93fb56a4475377ac
Marco Poetry Chen <marcopchen@gmail.com>
2018-04-22 22:06:10 -0400

    edited store and nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import DropDownMenu from 'material-ui/DropDownMenu';
+// import MenuItem from 'material-ui/MenuItem';
 
 const Nav = ({ counter }) => {
   counter = 4;
@@ -14,17 +14,10 @@ const Nav = ({ counter }) => {
             <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <DropDownMenu value={-1} onChange>
-              <MenuItem value={-1} primaryText='Products' />
-              <MenuItem value={1} primaryText='category A' />
-              <MenuItem value={2} primaryText='category B' />
-              <MenuItem value={3} primaryText='category C' />
-              <MenuItem value={4} primaryText='category D' />
-              <MenuItem value={5} primaryText='category E' />
-            </DropDownMenu>
+            <NavLink to='/products'>Products</NavLink>
           </li>
           <li>
-            <NavLink to='/cart'>Cart {counter}</NavLink>
+            <NavLink to='/cart'>Cart ({counter})</NavLink>
           </li>
           <li>
             <NavLink to='/user'>Account</NavLink>
