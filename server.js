ca19e728bbf8c5781d2b3556c1a16f0cdb310748
ajkatom <alon_adelson@yahoo.com>
2018-04-28 17:17:19 -0400

    seed file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require('./server/app');
 
-const db = require('./db');
+// const db = require('./db');
 
-db.syncAndSeed().then(() => {
-  console.log('seeded');
-});
+// db.syncAndSeed().then(() => {
+//   console.log('seeded');
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
