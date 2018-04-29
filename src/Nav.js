5cb8e0637743e7171b5e22a29e75164861ae0e62
ajkatom <alon_adelson@yahoo.com>
2018-04-29 16:15:56 -0400

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Navbar, NavbarBrand, Nav as _Nav, NavItem, NavLink as _NavLink } from 'reactstrap';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav as _Nav,
+  NavItem,
+  NavLink as _NavLink
+} from 'reactstrap';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DropDownMenu from 'material-ui/DropDownMenu';
@@ -35,38 +41,40 @@ class Nav extends Component {
 
   render() {
     const { value, counter } = this.state;
-    const { products } = this.props;
+    const { products, categories } = this.props;
     return (
       <MuiThemeProvider>
         <div>
           <Navbar color="light" light>
-          <NavbarBrand href="/#/">Grace Shopper</NavbarBrand>
-          <_Nav>
+            <NavbarBrand href="/#/">Grace Shopper</NavbarBrand>
+            <_Nav>
               <NavItem>
                 <DropDownMenu value={value} onChange={this.handleChange}>
                   <MenuItem
                     value={-1}
                     primaryText="products"
-                    containerElement={<Link to={'/products'} />}
+                    //containerElement={<Link to={'/products'} />}
                   />
-                  {products.length
-                    ? products.map(product => {
-                        return location.hash !== '#/products' ? (
+                  {categories.length
+                    ? categories.map(category => {
+                        return (
+                          //location.hash !== '#/products' ? (
                           <MenuItem
-                            key={product.id}
-                            value={product.id}
-                            primaryText={product.name}
-                            containerElement={
-                              <Link to={`/products/${product.id}`} />
-                            }
-                          />
-                        ) : (
-                          <MenuItem
-                            key={product.id}
-                            value={product.id}
-                            primaryText={product.name}
+                            key={category.id}
+                            value={category.id}
+                            primaryText={category.name}
+                            // containerElement={
+                            // <Link to={`/products/${product.id}`} />
+                            // }
                           />
                         );
+                        //  ) : (
+                        //     <MenuItem
+                        //       key={product.id}
+                        //       value={product.id}
+                        //       primaryText={product.name}
+                        //     />
+                        //   );
                       })
                     : null}
                 </DropDownMenu>
@@ -84,10 +92,11 @@ class Nav extends Component {
     );
   }
 }
-const mapStateToProps = ({ products, lineItems }) => {
+const mapStateToProps = ({ categories, products, lineItems }) => {
   return {
     products,
-    lineItems
+    lineItems,
+    categories
   };
 };
 
--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import AddToCart from './AddToCart';
 import { Container, Row, Col } from 'reactstrap';
 
 const Product = ({ product, history }) => {
   return product ? (
     <Container id={product.id}>
-      <h1>{product && product.name}</h1>
+      {history ? (
+        <NavLink
+          activeClassName="active"
+          className="disabled-link"
+          to={`/products/${product.id}`}
+        >
+          <h1>{product && product.name}</h1>
+        </NavLink>
+      ) : (
+        <NavLink activeClassName="active" to={`/products/${product.id}`}>
+          <h1>{product && product.name}</h1>
+        </NavLink>
+      )}
       <h5>{product.description}</h5>
-      <h2>${product && product.price}</h2> 
+      <h2>${product && product.price}</h2>
       <AddToCart product={product} history={history} />
     </Container>
   ) : null;
