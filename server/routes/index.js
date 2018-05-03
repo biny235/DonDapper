927895acb9a757afe43320999865277e243193bf
Marco Poetry Chen <marcopchen@gmail.com>
2018-05-03 18:13:47 -0400

    installed stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,6 @@ router.use('/orders', require('./orders'));
 router.use('/lineItems', require('./lineItems'));
 router.use('/users', require('./users'));
 router.use('/addresses', require('./addresses'));
-router.use('/google', require('./google'))
+// router.use('/google', require('./google'))
 
 module.exports = router;
