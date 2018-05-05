5fb0e8267941e77d5220114467550745a96a8032
Marco Poetry Chen <marcopchen@gmail.com>
2018-05-05 17:46:52 -0400

    added checkout

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,8 @@ router.use('/orders', require('./orders'));
 router.use('/lineItems', require('./lineItems'));
 router.use('/users', require('./users'));
 router.use('/addresses', require('./addresses'));
-router.use('/email', require('./email'))
+router.use('/email', require('./email'));
+router.use('/stripe', require('./stripe'));
 // router.use('/google', require('./google'))
 
 module.exports = router;
