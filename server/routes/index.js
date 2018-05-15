07f1ee746708891e7f6efd896b2ee48decd34c8f
biny235 <biny235@users.noreply.github.com>
2018-05-15 08:40:26 -0400

    fixing heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ router.use('/orders', require('./orders'));
 router.use('/lineItems', require('./lineItems'));
 router.use('/users', require('./users'));
 router.use('/addresses', require('./addresses'));
-router.use('/email', require('./email'));
+//router.use('/email', require('./email'));
 router.use('/google', require('./google'));
 router.use('/stripe', require('./stripe'));
 // router.use('/auth', require('./auth.js'))
